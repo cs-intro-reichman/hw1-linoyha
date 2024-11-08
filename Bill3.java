94e9c30a99909a276474e36966a0b9e947bc1772
linoyha <linoy.hadad@post.runi.ac.il>
2024-11-08 10:22:03 +0000

    finished

--- Bill3.java
+++ Bill3.java
@@ -5,7 +5,7 @@ public class Bill3 {
 		String name3 = args[2];
 		int sum = Integer.parseInt(args[3]);
 		double price = sum/3.0;
-		System.out.println("Dear " + name1 + ", " +name2 + " and " +name3 +": pay "+Math.ceil(price) 
+		System.out.println("Dear " + name3 + ", " +name2 + " and " +name1 +": pay "+Math.ceil(price) 
 		+" Shekels each.");
 	}
 }
